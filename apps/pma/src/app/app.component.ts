edb97043662d9074506cdbd63a95040ad02b5e21
rush <rush@rushs-mbp.lan>
2021-04-25 08:09:49 -0500

    initial commit

---
+++ apps/pma/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pma-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'pma';
+}
